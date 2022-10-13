dfcbb9c6a06a03f6d7b015e8b309b0ce88d2d97a
Bassam <basmaym148@gmail.com>
2022-10-13 14:29:12 -0400

    figuring out the required Classes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
 <script>
+class Card{
+  constructor(q, a, id) {
+    this.id = id
+    this.question = q
+    this.answer = a
+  }
+}
+class CardDeck{
+  constructor(name, creator) {
+    this.name = name
+    this.creator = creator
+    this.cards = []
+  }
+  newCard(question, answer) {
+    this.cards.push(new Card(question, answer, this.cards.length + 1))
+  }
+  updateIds() {
+    this.cards = this.cards.filter(card => card != undefined)
+    this.cards.forEach((x, i) => {
+      x.id = i + 1
+    })
+  }
+  deleteCard(id) {
+    this.cards = this.cards.map(card => {
+      if (card.id != id){
+        return card
+      }
+    })
+    this.updateIds()
+  }
+  moveCard(id, position) {
+      let card = this.cards.filter(x => x.id == id)[0]
+      this.deleteCard(id)
+      this.cards.splice(position - 1, 0, card)
+      this.updateIds()
+  }
+}
 import Welcome from './components/Welcome.vue'
+import DisplayCard from './components/DisplayCard.vue';
   export default {
     data() {
         return {
@@ -27,7 +65,7 @@ import Welcome from './components/Welcome.vue'
             }
         }
     },
-    components: { Welcome }
+    components: { Welcome, DisplayCard }
 }
 </script>
 
@@ -39,8 +77,12 @@ import Welcome from './components/Welcome.vue'
       User = x
     }"/>
 
-    <div v-if="!renderWelcome">
+    <div v-if="!renderWelcome" style="width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; flex-wrap: wrap;">
       <h1 class="fade" v-if="renderHello">Welcome {{User}}!</h1>
+      <div v-if="!renderHello" style="width: 100vw; height: 100vh; display: flex; align-items: center; justify-content: center; flex-wrap: wrap;">
+        <DisplayCard />
+        <DisplayCard />
+      </div>
     </div>
 
   </div>
---
+++ src/components/DisplayCard.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        name: "DisplayCard",
+    }
+</script>
+
+<template>
+    <div class="displaycard">
+        
+    </div>
+</template>
+
+<style>
+    .displaycard{
+        width: 40%;
+        height: 60%;
+        background-color: rgb(189, 0, 189);
+        border: .7vh solid purple;
+        border-radius: 3vh;
+        cursor: pointer;
+        margin: 5vh;
+    }
+</style>
